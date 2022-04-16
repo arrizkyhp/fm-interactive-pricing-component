81bbc60617e6b54289591921d88706e908d2378f
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2022-04-16 15:01:19 +0700

    update html report

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     <div className=" h-[50vh] md:h-[95vh] bg-[url('assets/images/bg-pattern.svg')] bg-no-repeat bg-very-pale-blue bg-cover md:bg-contain">
       <main className='max-w-xl text-5xl font-manrope mx-auto w-full pt-14 px-5 md:px-0  mb-10'>
           <section className='bg-[url("assets/images/pattern-circles.svg")] bg-no-repeat bg-center py-12 mb-5 md:mb-10 text-center flex flex-col items-center'>
-            <h1 className='text-dark-desaturated-blue text-[1.3rem] md:text-xl font-extrabold mb-4'>Simple, traffic-based pricing</h1>
+            <p className='text-dark-desaturated-blue text-[1.3rem] md:text-xl font-extrabold mb-4'>Simple, traffic-based pricing</p>
             <p className='text-default font-semibold md:font-bold text-grayish-blue max-w-[200px] md:max-w-none w-full leading-6'>Sign-up for our 30-day trial. No credit card required. </p>
           </section>
           <Card />
